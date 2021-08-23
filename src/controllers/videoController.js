b8fef70f87c6e58d635d72d6895b0fc45e91d7e2
empodi <kglein9513@gmail.com>
2021-08-23 11:20:24 +0900

    added view - pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Video:${req.params.id}`);
-}
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+
 export const edit = (req, res) => {
     console.log(req.params);
     return res.send("Edit");
