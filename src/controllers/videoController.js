8e76c241849fed9491242628e110e562b52b7010
empodi <kglein9513@gmail.com>
2021-08-24 21:18:11 +0900

    set mongodb

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,47 +1,35 @@
-let videos= [
-    {
-        title:"First Video",
-        rating:5,
-        comments:3,
-        createdAt: "2 minutes ago",
-        views:1,
-        id:1
-    },
-    {
-        title:"Second Video",
-        rating:5,
-        comments:3,
-        createdAt: "2 minutes ago",
-        views:59,
-        id:2
-    },
-    {
-        title:"Third Video",
-        rating:5,
-        comments:3,
-        createdAt: "2 minutes ago",
-        views:59,
-        id:3
-    }
-];
+import Video from "../models/Video";
+import videoRouter from "../routers/videoRouter";
 
-export const trending = (req, res) => {
-    return res.render("home", {pageTitle:"Home", videos});
-}
+//Video.find({}, (error, videos) => {});
+
+export const home = async(req, res) => {
+    try {
+        const videos = await Video.find({});
+        return res.render("home", {pageTitle:"Home", videos});
+    } catch {
+        return res.send("server-error");
+    }
+};
 export const watch = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render("watch", {pageTitle:`Watching: ${video.title}`, video});
-}
+    return res.render("watch", {pageTitle:"Watching"});
+};
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render("edit", {pageTitle:`Editing: ${video.title}`, video});
-}
+
+    return res.render("edit", {pageTitle:"Editing"});
+};
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
-}
+};
+export const getUpload = (req, res) => {
+    return res.render("upload", {pageTitle:"Upload Video"});
+};
+export const postUpload = (req, res) => {
+    const {title} = req.body;
+    return res.redirect("/");
+};
 
