70e2edb3efb8aefd55a8c45c8f6fa0444706ec1c
empodi <kglein9513@gmail.com>
2021-08-28 17:48:21 +0900

    added env

--- src/server.js
+++ src/server.js
@@ -20,11 +20,11 @@ app.use(express.urlencoded({extended:true}));
 //session should be initialized before routers
 app.use(
     session({
-        secret: "Hello!",
+        secret: process.env.COOKIE_SECRET,
         resave: false,
         saveUninitialized: false,
         store: MongoStore.create({
-            mongoUrl: "mongodb://127.0.0.1:27017/wetube",
+            mongoUrl: process.env.DB_URL,
         })
     })
 );
