5d54cda58f573ab5ea0cde8ef6d8a6dbe17a8b4e
empodi <kglein9513@gmail.com>
2021-08-30 17:55:58 +0900

    update login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import { edit, remove, logout, see, startGithubLogin, finishGithubLogin } from "../controllers/userController";
 
 const userRouter = express.Router();
 
@@ -7,5 +7,7 @@ userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
 userRouter.get("/:id", see);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
