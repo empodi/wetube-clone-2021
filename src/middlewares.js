909e1861d39677aed056acff862d1df9f4858c00
empodi <kglein9513@gmail.com>
2021-09-06 15:33:15 +0900

    updated protection middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -7,3 +7,21 @@ export const localsMiddleware = (req, res, next) => {
     //console.log("locals", res.locals);
     next();
 }
+
+export const protectorMiddleware = (req, res, next) => { // only logged in user can go to edit-profile page
+
+    if (req.session.loggedIn === true) {
+        return next();
+    } else {
+        return res.redirect("/login");
+    }
+}
+
+export const publicOnlyMiddleware = (req, res, next) => { // if you're logged in, you don't have to go to log-in page
+
+    if (!req.session.loggedIn) {
+        return next();
+    } else {
+        return res.redirect("/");
+    }
+}
